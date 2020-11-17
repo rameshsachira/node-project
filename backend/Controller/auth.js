f935bbb5a6d36ca9ce267bc8b0467a142cf8fc7c
Ramesh Sachira <rameshsachira@gmail.com>
2020-11-17 10:13:17 +0530

    convert callbacks to promises

--- backend/Controller/auth.js
+++ backend/Controller/auth.js
@@ -18,89 +18,67 @@ const { rejects } = require('assert');
 // }
 
 
-exports.signup =  (req, res,) => {
-  console.log(req.body);
+exports.signup = async (req, res,) => {
   try {
-    const {email,password,firstName,lastName} = req.body;
+    const { email, password, firstName, lastName } = req.body;
 
-    let user =  User.findOne({email}).exec().then;
-
+    let user = await User.findOne({ email: email }).exec();
     // Make sure this account doesn't already exist
     if (user) {
       return res
         .status(400)
-        .json({ error: "User with this email already exists..!" });
+        .json({ error: "User with this email already exists..! " });
     }
-    // Create and save the user
-    user = new User({
-      firstName:firstName,
-      lastName:lastName,
-      email:email,
-      password:password,
+    else {
+      // Create and save the user
+      client = new User({
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: password,
+      })
+    };
+    let newClient = await client.save();
+    res.send(newClient);
+
+    // Create a verification token for this user
+    var token = new Token({
+      _userId: client._id,
+      token: crypto.randomBytes(16).toString("hex"),
     });
-    user.save().then(
-      ()=> {
-        res.status(201).json({
-          message: 'User added successfully!'
-        });
+    // Save the verification token
+    await token.save();
+
+    // Send the email
+    var transporter = nodemailer.createTransport({
+      service: "gmail",
+      port: 587,
+      auth: {
+        user: process.env.SENDGRID_USERNAME,
+        pass: process.env.SENDGRID_PASSWORD,
+      },
+    });
+
+    var mailOptions = {
+      from: "no-reply@testapplication.com",
+      to: client.email,
+      subject: "Account Verification Token",
+      text:
+        "Hello,\n\n" +
+        "Please verify your account by clicking the link: \nhttp://" +
+        // req.headers.host +
+        "/confirmation/" +
+        token.token +
+        ".\n",
+    };
+    transporter.sendMail(mailOptions, function (err) {
+      if (err) {
+        return res.status(500).send({ msg: err.message });
       }
-      ).catch(
-        (error) => {
-          res.status(500).json({
-            error: error
-          });
-        }
-      );
-  
-      // Create a verification token for this user
-      var token = new Token({
-        _userId: user._id,
-        token: crypto.randomBytes(16).toString("hex"),
-      });
-
-      // Save the verification token
-      token.save().then(
-        ()=> {
-          res.status(201).json({
-            message: 'Token created successfully!'
-          });
-        }
-        ).catch(
-          (error) => {
-            res.status(500).json({
-              error: error
-            });
-          }
-        );
-
-        // Send the email
-        var transporter = nodemailer.createTransport({
-          service: "gmail",
-          auth: {
-            user: process.env.SENDGRID_USERNAME,
-            pass: process.env.SENDGRID_PASSWORD,
-          },
-        });
-        var mailOptions = {
-          from: "no-reply@testapplication.com",
-          to: user.email,
-          subject: "Account Verification Token",
-          text:
-            "Hello,\n\n" +
-            "Please verify your account by clicking the link: \nhttp://" +
-            req.headers.host +
-            "/confirmation/" +
-            token.token +
-            ".\n",
-        };
-        transporter.sendMail(mailOptions, function (err) {
-          if (err) {
-            return res.status(500).send({ msg: err.message });
-          }
-          res
-            .status(200)
-            .send("A verification email has been sent to " + user.email + ".");
-        });
+      res
+        .status(200)
+        .send("A verification email has been sent");
+    });
 
   } catch (error) {
     console.log(error.message);
@@ -109,20 +87,37 @@ exports.signup =  (req, res,) => {
 };
 
 
-exports.login = (req, res) => {
 
-  User.findOne({ email: req.body.email }, function (err, user) {
-    if (!user) return res.status(401).send({ msg: 'The email address ' + req.body.email + ' is not associated with any account.' });
 
-    user.comparePassword(req.body.password, function (err, isMatch) {
-      if (!isMatch) return res.status(401).send({ msg: 'Invalid email or password' });
-
-      // Make sure the user has been verified
-      if (!user.isVerified) return res.status(401).send({ type: 'not-verified', msg: 'Your account has not been verified.' });
-
-      res.send({ token: generateToken(user), user: user.toJSON() });
-    });
-  });
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    let user = await User.findOne({ email: email });
+    console.log(user);
+    if (!user) {
+      return res
+        .status(400)
+        .json({ error: 'The email address ' + user + ' is not associated with any account.' });
+    } else {
+      const isMatched = await customer.compare(password, user.password);
+      if (!isMatched) {
+        return res
+          .status(400)
+          .json({ error: 'Incorrect password or Username.' });
+      }else{
+        return res
+          .status(201)
+          .json({ msg: 'Log In Successful.' });
+      }
+    }
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({ msg: "Server Error...." });
+  }
 };
+
+
+
+
 
 
